5ce62879fe4b495d1b2af1e7b8e319592cbe38bd
netdex <admin@netdex.org>
2023-08-15 21:43:33 -0500

    make: build with makefile

---
+++ Makefile
@@ -0,0 +1,10 @@
+ARDUINO_LIBS = TrinketHidCombo
+
+ALTERNATE_CORE = Digispark
+BOARD_TAG = digispark-tiny
+BOARD_SUB = default
+VARIANT = digispark
+F_CPU = 16500000L
+ISP_PROG = micronucleus
+
+include third-party/arduino-mk/Arduino.mk
---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIGISTUMP_PATH=C:/Users/$USER/AppData/Local/Arduino15/packages/digistump
+export ARDUINO_DIR=C:/Users/$USER/AppData/Local/Arduino15/packages/arduino
+export ARDUINO_SKETCHBOOK=C:/Users/$USER/Documents/Arduino
+export AVR_TOOLS_DIR=C:/Users/$USER/AppData/Local/Arduino15/packages/arduino/tools/avr-gcc/7.3.0-atmel3.6.1-arduino7
+export ALTERNATE_CORE_PATH=$DIGISTUMP_PATH/hardware/avr/1.7.5
+export MICRONUCLEUS_PATH=$DIGISTUMP_PATH/tools/micronucleus/2.6
